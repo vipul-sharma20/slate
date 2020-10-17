9128a55db1ea9099b24e1be6eae075a162e7f2ce
Vipul Sharma <vipul.sharma20@gmail.com>
2020-10-18 00:31:28 +0530

    add: block kit templates

--- app/constants.py
+++ app/constants.py
@@ -1,5 +1,12 @@
 import os
 
+ALL = "all"
+ACTIVE = "active"
+INACTIVE = "inactive"
+
+CAT_API_HOST = "https://api.thecatapi.com"
+
+NO_USER_ERROR_MESSAGE = "Your user details or standup for your user doesn't exist in the database."
 
 STANDUP_CHANNEL_ID = os.environ.get("STANDUP_CHANNEL_ID")
 
@@ -13,6 +20,36 @@ STANDUP_SECTION_DIVIDER = {"type": "divider"}
 STANDUP_USER_SECTION = {"type": "section", "text": {"type": "mrkdwn", "text": ""}}
 
 
-ALL = "all"
-ACTIVE = "active"
-INACTIVE = "inactive"
+NOTIFICATION_BLOCKS = [
+    {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": "Hello :wave: It's time to start your daily standup",
+        },
+    },
+    {
+        "type": "actions",
+        "elements": [
+            {
+                "type": "button",
+                "text": {"type": "plain_text", "emoji": True, "text": "Open Dialog"},
+                "style": "primary",
+                "value": "open_dialog",
+            }
+        ],
+    },
+]
+
+
+SUBMIT_TEMPLATE_SECTION_1 = {
+    "type": "section",
+    "text": {"type": "mrkdwn", "text": "Thanks for submitting today's standup :slightly_smiling_face:"}
+}
+
+
+SUBMIT_TEMPLATE_SECTION_2 = {
+    "type": "section",
+    "text": {"type": "mrkdwn", "text": "Here's a cat for you"}
+}
+
