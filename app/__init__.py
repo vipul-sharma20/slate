23e554e65467f128dce18b67469ddff7ee760dfc
Vipul Sharma <vipul.sharma20@gmail.com>
2021-07-25 01:33:08 +0530

    add: app config to init

--- app/__init__.py
+++ app/__init__.py
@@ -19,6 +19,18 @@ signature_verifier = SignatureVerifier(os.environ["SLACK_SIGNING_SECRET"])
 app_cache = Cache()
 
 
+class Config:
+    # General Config
+    # SECRET_KEY = environ.get('SECRET_KEY')
+    # FLASK_APP = environ.get('FLASK_APP')
+    # FLASK_ENV = environ.get('FLASK_ENV')
+
+    # Database
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
 # Custom encoder to serialize datetime objects
 class StandupJSONEncoder(JSONEncoder):
     def default(self, obj):
@@ -29,9 +41,11 @@ class StandupJSONEncoder(JSONEncoder):
 
 
 def create_app():
-    """Construct the core application."""
+    """
+    Construct the core application.
+    """
     app = Flask(__name__, instance_relative_config=False)
-    app.config.from_object('config.Config')
+    app.config.from_object(Config())
     app.json_encoder = StandupJSONEncoder
 
     db.init_app(app)
